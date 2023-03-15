3ae683c26ea86904e1a77f135f6ea8a4ce5b4218
chay <chaithanya.puli1994@gmail.com>
2023-03-15 12:37:26 +1100

    weed03

--- pageObject/config/config_xlsx.py
+++ pageObject/config/config_xlsx.py
@@ -3,7 +3,7 @@ import xlrd
 
 class ConfigXlsxData:
     #open_woekbook will oopen the entire excel file
-    book = xlrd.open_workbook(r'C:\Users\chaitanya\Documents\jupitor-planit\pageObject\excel\sample.xlsx')
+    book = xlrd.open_workbook("../excel/sample.xlsx")
     #sheet_by_name() it take one argument (str) the sheet name which we need to read
     sheet1 = book.sheet_by_name('Sheet1')
     # nrows is function which is not callable it will return number of rows
