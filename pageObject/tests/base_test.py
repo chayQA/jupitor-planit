18574b18e6c8bb0568b67fa92f8f0dd50eeef08f
chay <chaithanya.puli1994@gmail.com>
2023-03-18 14:54:03 +1100

    relative005

--- pageObject/tests/base_test.py
+++ pageObject/tests/base_test.py
@@ -1,21 +1,23 @@
 import pytest
 from pageObject.config.config_file import ConfigData
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
 
 @pytest.mark.usefixtures('init_driver')
 class BaseTest:
 
     @pytest.fixture(scope='function')
     def init_driver(self, request):
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--no-sandbox")
-        options.binary_location
 
-        web_driver = webdriver.Chrome(options =options) 
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         request.cls.driver = web_driver
         yield
         web_driver.quit()
