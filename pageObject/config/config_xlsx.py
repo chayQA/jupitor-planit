7d4ae31420a38ca10b618c551f4a376238d31fb2
chay <chaithanya.puli1994@gmail.com>
2023-03-18 14:12:27 +1100

    relative

--- pageObject/config/config_xlsx.py
+++ pageObject/config/config_xlsx.py
@@ -1,9 +1,13 @@
 import xlrd
+import os
+dir = os.path.dirname("_file_")
+filename = os.path.join(dir, "../excel/sample.xlsx")
+
 #install xlrd
 
 class ConfigXlsxData:
     #open_woekbook will oopen the entire excel file
-    book = xlrd.open_workbook("../excel/sample.xlsx")
+    book = xlrd.open_workbook(filename)
     #sheet_by_name() it take one argument (str) the sheet name which we need to read
     sheet1 = book.sheet_by_name('Sheet1')
     # nrows is function which is not callable it will return number of rows
